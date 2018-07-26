614e3c031abba57ad9534383a2e00aa599803a70
Ayobamidele Ayeko <ayobamideleayeko@AyobamidelesMBP.Dual-Band-Broadband-Modem>
2018-07-26 17:48:55 +0100

    ...

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express 	= 	require('express'),
-	mongoose 	= 	require('mongoose'),
-	bodyParser 	= 	require('body-parser'),
-	Seed      	= 	require('./models/seeds.js'),
-	faker		=	require('faker'),
-	multer		=	require('multer'),
-	seedDB		=	require('./seed');
+var express = require('express'),
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser'),
+	Seed = require('./models/seeds.js'),
+	faker = require('faker'),
+	multer = require('multer'),
+	seedDB = require('./seed');
 
 var app = express();
 
@@ -12,20 +12,25 @@ var app = express();
 mongoose.connect("mongodb://localhost/seedersblock");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(express.static(__dirname + "/public"));
 app.use('/scripts', express.static(__dirname + '/node_modules/trumbowyg/dist/'));
 
-// Configure multer
-var upload = multer({ storage: storage });
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, __dirname+'/public/file/uploads/');
+		cb(null, __dirname + '/public/file/uploads/');
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.fieldname + '-' + Date.now());
 	}
+});
+
+// Configure multer
+var upload = multer({
+	storage: storage
 });
 
 // Set EJS as default view engine
@@ -66,20 +71,19 @@ app.get('/seeds/new', function (req, res) {
 // CREATE ROUTE
 app.post('/seeds', upload.any(), function (req, res) {
 	if (!req.files) {
-    console.log("No file received");
-    return res.send({
-      success: false
-    });
+		console.log("No file received");
+		return res.send({
+			success: false
+		});
 
-  } else {
-    console.log('file received');
-    return res.send({
-      success: true
-    })
-  }
+	} else {
+		console.log('file received');
+		return res.send({
+			success: true
+		})
+	}
 });
 
 app.listen(3000, function () {
 	console.log("Started Seedersblock app...");
 });
-
