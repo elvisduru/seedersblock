ef84e0d884923abae5176541736014cfa3a005ed
Elvis Duru <duruelvis95@gmail.com>
2018-07-27 17:59:45 +0100

    started comment model

--- app.js
+++ app.js
@@ -79,7 +79,6 @@ app.post('/seeds', upload.single('featuredImg'), function (req, res) {
 	res.set('X-XSS-Protection', 0);
 	var host = req.headers.host;
 	var prefix = 'file/uploads/';
-	var filePath = req.protocol + "://" + host + '/' + prefix + req.file.filename;
 
 	// xss validation
 	req.body.seed.title = sanitizeHtml(req.body.seed.title);
@@ -101,7 +100,6 @@ app.post('/seeds', upload.single('featuredImg'), function (req, res) {
 			avatar: faker.image.avatar()
 		},
 		title: req.body.seed.title,
-		image: filePath,
 		body: req.body.seed.content,
 		excerpt: req.body.seed.excerpt,
 		category: req.body.seed.category,
@@ -120,11 +118,13 @@ app.post('/seeds', upload.single('featuredImg'), function (req, res) {
 
 	} else {
 		console.log('file received');
+		var filePath = req.protocol + "://" + host + '/' + prefix + req.file.filename;
+		seed.image = filePath;
 		Seed.create(seed, function (err, createdSeed) {
 			if (err) {
 				console.log(err);
 			} else {
-				return res.redirect('/seeds');
+				return res.redirect('/seeds/' + createdSeed._id);
 			}
 		});
 	}
@@ -154,6 +154,49 @@ app.delete('/seeds/:id/delete', function(req, res) {
 	});
 });
 
+// Edit Route
+app.get('/seeds/:id/edit', function(req, res) {
+	Seed.findById(req.params.id, function(err, foundSeed) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('seeds/edit', {seed: foundSeed});
+		}
+	});
+});
+
+
+app.put('/seeds/:id', function(req, res) {
+	res.set('X-XSS-Protection', 0);
+
+	// xss validation
+	req.body.seed.title = sanitizeHtml(req.body.seed.title);
+	req.body.seed.content = sanitizeHtml(req.body.seed.content, {
+		allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+			'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+			'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe', 'img', 'br', 'hr', 'audio', 'video'
+		],
+		allowedAttributes: false,
+		allowedIframeHostnames: ['www.youtube.com', 'player.vimeo.com']
+	});
+	req.body.seed.excerpt = sanitizeHtml(req.body.seed.excerpt);
+
+	var body = req.body.seed;
+
+	var seed = {
+		title: req.body.seed.title,
+		body: req.body.seed.content,
+		excerpt: req.body.seed.excerpt
+	};
+
+	Seed.findByIdAndUpdate(req.params.id, seed, function(err, updatedSeed) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/seeds/' + updatedSeed._id);
+		}
+	});
+});
 
 app.listen(3000, function () {
 	console.log("Started Seedersblock app...");
