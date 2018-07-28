3d37d25afa2343bab9dd90c1a17015c3531ef55b
Ayobamidele Ayeko <ayobamideleayeko@AyobamidelesMBP.Dual-Band-Broadband-Modem>
2018-07-28 16:45:42 +0100

    ...

--- public/asset/js/index.js
+++ public/asset/js/index.js
@@ -8,45 +8,45 @@ function formatStr(str) {
 	}
 }
 
-$(".formatcount").each(function(i) {
+$(".formatcount").each(function (i) {
 	$(this).text(formatStr($(this).text()));
 });
 
 $('#editor').trumbowyg({
 	btnsDef: {
-        // Create a new dropdown
-        image: {
-            dropdown: ['insertImage', 'upload'],
-            ico: 'insertImage'
-        }
-    },
-    btns: [
-        ['viewHTML'],
-        ['undo', 'redo'], // Only supported in Blink browsers
-        ['formatting'],
-        ['strong', 'em', 'del'],
-        ['foreColor', 'backColor'],
-        ['superscript', 'subscript'],
-        ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-        ['unorderedList', 'orderedList'],
-        ['horizontalRule'],
-        ['table'],
-        ['removeformat'],
-        ['link'],
-        ['image', 'insertAudio'],
-        ['fullscreen']
-    ],
-    plugins: {
-        // Add imagur parameters to upload plugin for demo purposes
-        upload: {
-            serverPath: 'https://api.imgur.com/3/image',
-            fileFieldName: 'image',
-            urlPropertyName: 'data.link',
-            headers: {
-            	'Authorization': 'Client-ID 82ba68ef5d815bb'
-            }
-        }
-    },
+		// Create a new dropdown
+		image: {
+			dropdown: ['insertImage', 'upload'],
+			ico: 'insertImage'
+		}
+	},
+	btns: [
+		['viewHTML'],
+		['undo', 'redo'], // Only supported in Blink browsers
+		['formatting'],
+		['strong', 'em', 'del'],
+		['foreColor', 'backColor'],
+		['superscript', 'subscript'],
+		['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+		['unorderedList', 'orderedList'],
+		['horizontalRule'],
+		['table'],
+		['removeformat'],
+		['link'],
+		['image', 'insertAudio'],
+		['fullscreen']
+	],
+	plugins: {
+		// Add imagur parameters to upload plugin for demo purposes
+		upload: {
+			serverPath: 'https://api.imgur.com/3/image',
+			fileFieldName: 'image',
+			urlPropertyName: 'data.link',
+			headers: {
+				'Authorization': 'Client-ID 82ba68ef5d815bb'
+			}
+		}
+	},
 	autogrow: true,
 	autogrowOnEnter: true,
 	imageWidthModalEdit: true,
@@ -60,11 +60,11 @@ $('#post-form').submit(function () {
 });
 
 function createExcerpt() {
-	
+
 	$('#excerpt').val($('#editor').trumbowyg('html').substring($('#editor').trumbowyg('html').indexOf("<p>"), $('#editor').trumbowyg('html').indexOf("</p>")).replace(/<p>/g, "").replace(/<\/p>/g, "").replace(/<br>/g, ""));
 }
 
 // initialize materialized select
-$(document).ready(function(){
+$(document).ready(function () {
 	$('select').formSelect();
 });
