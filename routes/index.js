65c51ca178ff1e283318cb40fc34ca12c40c9249
Elvis Duru <duruelvis95@gmail.com>
2018-07-31 11:12:27 +0100

    changed dashboard to settings

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function (req, res) {
 });
 
 //Dasboard Route 
-router.get('/dashboard',function (req, res){
-	res.render("dashboard");
+router.get('/settings',function (req, res){
+	res.render("settings");
 });
 
 module.exports = router;
